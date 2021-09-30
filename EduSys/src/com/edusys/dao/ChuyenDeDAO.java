a8ef7099364e047481a547826269e9a5da9f0a07
haotn <haotndev@gmail.com>
2021-09-30 15:02:52 +0700

    Cap nhat project

--- EduSys/src/com/edusys/dao/ChuyenDeDAO.java
+++ EduSys/src/com/edusys/dao/ChuyenDeDAO.java
@@ -6,42 +6,87 @@
 package com.edusys.dao;
 
 import com.edusys.entity.ChuyenDe;
+import com.edusys.helper.Xjdbc;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author haotn
  */
-public class ChuyenDeDAO extends EduSysDAO<ChuyenDe, String>{
+public class ChuyenDeDAO extends EduSysDAO<ChuyenDe, String> {
+
+    String INSERT_SQL = "INSERT INTO CHUYENDE(MACD, TENCD, HOCPHI, THOILUONG, HINH, MOTA) VALUES (?, ?, ?, ?, ?, ?)";
+    String UPDATE_SQL = "UPDATE CHUYENDE SET TENCD = ?, HOCPHI = ?, THOILUONG = ?, HINH=?  WHERE MACD = ?";
+    String DELETE_SQL = "DELETE FROM CHUYENDE WHERE MACD = ?";
+    String SELECT_ALL_SQL = "SELECT * FROM CHUYENDE";
+    String SELECT_BY_ID_SQL = "SELECT * FROM CHUYENDE WHERE MACD = ?";
 
     @Override
     public void insert(ChuyenDe entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Xjdbc.update(INSERT_SQL, entity.getMaCD(), entity.getTenCD(), entity.getHocPhi(), entity.getThoiLuong(), entity.getHinhCD(), entity.getMoTa());
+        } catch (SQLException ex) {
+            Logger.getLogger(ChuyenDeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void update(ChuyenDe entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Xjdbc.update(UPDATE_SQL, entity.getTenCD(), entity.getHocPhi(), entity.getThoiLuong(), entity.getHinhCD(), entity.getTenCD(), entity.getMaCD());
+        } catch (SQLException ex) {
+            Logger.getLogger(ChuyenDeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
-    public void delete(String key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void delete(String id) {
+        try {
+            Xjdbc.update(DELETE_SQL, id);
+        } catch (SQLException ex) {
+            Logger.getLogger(ChuyenDeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public List<ChuyenDe> selectAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.selectBySql(SELECT_ALL_SQL);
     }
 
     @Override
-    public ChuyenDe selectById(String key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ChuyenDe selectById(String id) {
+        List<ChuyenDe> list = this.selectBySql(SELECT_BY_ID_SQL, id);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
     }
 
     @Override
     public List<ChuyenDe> selectBySql(String sql, Object... args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<ChuyenDe> list = new ArrayList<ChuyenDe>();
+        try {
+            ResultSet result = Xjdbc.query(sql, args);
+            while (result.next()) {
+                ChuyenDe entity = new ChuyenDe();
+                entity.setMaCD(result.getString("MACD"));
+                entity.setTenCD(result.getString("TENCD"));
+                entity.setHocPhi(result.getFloat("HOCPHI"));
+                entity.setThoiLuong(result.getInt("THOILUONG"));
+                entity.setHinhCD(result.getString("HINH"));
+                entity.setMoTa(result.getString("MOTA"));
+                list.add(entity);
+            }
+            result.getStatement().getConnection().close();
+            return list;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-    
+
 }
