f4cb85dd68cfcea9668f8ef560a452ebee57b61b
クォック <90124425+Kuokku48@users.noreply.github.com>
2021-10-07 22:48:00 +0700

    NguoiHoc

---
+++ EduSys/src/com/edusys/dao/NguoiHocDAO.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edusys.dao;
+
+import com.edusys.entity.NguoiHoc;
+import com.edusys.helper.Xjdbc;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author QUOC DAO
+ */
+public class NguoiHocDAO extends EduSysDAO<NguoiHoc, String> {
+
+    String INSERT_SQL = "INSERT INTO NGUOIHOC(MANH, HO, TEN, GIOITINH, NGAYSINH, DIENTHOAI, EMAIL, GHICHU, MANV, NGAYDK)"
+           + "VALUES(?,?,?,?,?,?,?,?,?,?)";
+    String UPDATE_SQL = "UPDATE NGUOIHOC SET HO=?, TEN=?, GIOITINH=?, NGAYSINH=?, DIENTHOAI=?, EMAIL=?, GHICHU=?, MANV=? , NGAYDK=? WHERE MANV=?";
+    String DELETE_SQL = "DELETE FROM NGUOIHOC WHERE MANH=?";
+    String SELECT_ALL_SQL = "SELECT * FROM NGUOIHOC";
+    String SELECT_BY_ID_SQL = "SELECT * FROM NGUOIHOC WHERE MANH=?";
+    
+    @Override
+    public void insert(NguoiHoc entity) {
+        try {
+            Xjdbc.update(INSERT_SQL, entity.getMaNH(), entity.getHo(), entity.getTen(), entity.getGioiTinh(), entity.getNgaySinh(),
+                            entity.getDienThoai(), entity.getEmail(), entity.getGhiChu(), entity.getMaNV(), entity.getNgayDK());
+        }
+        catch(Exception ex) {
+            Logger.getLogger(ChuyenDeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void update(NguoiHoc entity) {
+        try {
+            Xjdbc.update(UPDATE_SQL, entity.getHo(), entity.getTen(), entity.getGioiTinh(), entity.getNgaySinh(), 
+                            entity.getDienThoai(),entity.getEmail(), entity.getGhiChu(), entity.getMaNV(), entity.getNgayDK());
+        }
+        catch(Exception ex) {
+            Logger.getLogger(ChuyenDeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void delete(String id) {
+        try {
+            Xjdbc.update(DELETE_SQL, id);
+        }
+        catch(Exception ex){
+            Logger.getLogger(ChuyenDeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public List<NguoiHoc> selectAll() {
+       return this.selectBySql(SELECT_ALL_SQL);
+    }
+
+    @Override
+    public NguoiHoc selectById(String id) {
+        List<NguoiHoc> list = this.selectBySql(SELECT_BY_ID_SQL, id);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    @Override
+    protected List<NguoiHoc> selectBySql(String sql, Object... args) {
+        List<NguoiHoc> list = new ArrayList<NguoiHoc>();
+        try{
+            ResultSet result = Xjdbc.query(sql, args);
+            while (result.next()) {
+                NguoiHoc entity = new NguoiHoc();
+                entity.setMaNH(result.getString("MANH"));
+                entity.setHo(result.getString("HO"));
+                entity.setTen(result.getString("TEN"));             
+                entity.setGioiTinh(result.getBoolean("GIOITINH"));
+                entity.setNgaySinh(result.getDate("NGAYSINH"));
+                entity.setDienThoai(result.getString("DIENTHOAI"));
+                entity.setEmail(result.getString("EMAIL"));
+                entity.setGhiChu(result.getString("GHICHU"));
+                entity.setMaNV(result.getString("MANV"));
+                entity.setNgayDK(result.getDate("NGAYDK"));
+            }
+            return list;
+        }
+        catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+    
+}
---
+++ EduSys/src/com/edusys/entity/NguoiHoc.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edusys.entity;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author QUOC DAO
+ */
+public class NguoiHoc {
+    private String MaNH;	
+    private String Ho;
+    private String Ten;
+    private Boolean gioiTinh;
+    private Date ngaySinh;
+    private String dienThoai;
+    private String Email;
+    private String ghiChu;
+    private String MaNV;
+    private Date NgayDK;	
+    
+    public NguoiHoc() {}
+	
+    public NguoiHoc(String MaNH, String Ho, String Ten, Boolean gioiTinh, Date ngaySinh, String dienThoai, String Email, String ghiChu, String MaNV, Date NgayDK) {
+        this.MaNH = MaNH;
+        this.Ho = Ho;
+        this.Ten = Ten;
+        this.gioiTinh = gioiTinh;
+        this.ngaySinh = ngaySinh;
+        this.dienThoai = dienThoai;
+        this.Email = Email;
+        this.ghiChu = ghiChu;
+        this.MaNV = MaNV;
+        this.NgayDK = NgayDK;
+    }
+
+    public String getMaNH() {
+        return MaNH;
+    }
+
+    public void setMaNH(String MaNH) {
+        this.MaNH = MaNH;
+    }
+
+    public String getHo() {
+        return Ho;
+    }
+
+    public void setHo(String Ho) {
+        this.Ho = Ho;
+    }
+
+    public String getTen() {
+        return Ten;
+    }
+
+    public void setTen(String Ten) {
+        this.Ten = Ten;
+    }
+
+    public Boolean getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(Boolean gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public Date getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(Date ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getDienThoai() {
+        return dienThoai;
+    }
+
+    public void setDienThoai(String dienThoai) {
+        this.dienThoai = dienThoai;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getGhiChu() {
+        return ghiChu;
+    }
+
+    public void setGhiChu(String ghiChu) {
+        this.ghiChu = ghiChu;
+    }
+
+    public String getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(String MaNV) {
+        this.MaNV = MaNV;
+    }
+
+    public Date getNgayDK() {
+        return NgayDK;
+    }
+
+    public void setNgayDK(Date NgayDK) {
+        this.NgayDK = NgayDK;
+    }
+}
